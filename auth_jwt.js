6f1fb4fc21323b16cd88c0d08b34cd5d6a22233b
Younes-Yassine <156845678+Younes-Yassine@users.noreply.github.com>
2025-03-02 23:23:33 -0700

    Update auth_jwt.js

--- auth_jwt.js
+++ auth_jwt.js
@@ -1,20 +1,24 @@
+require('dotenv').config(); // Ensure env vars are loaded if this file is run independently
 var passport = require('passport');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
+var db = require('./db'); // Modified: using singleton instance
 
 var opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
-opts.secretOrKey = process.env.SECRET_KEY;
+// Use UNIQUE_KEY from your .env as the secret key
+opts.secretOrKey = process.env.UNIQUE_KEY;
 
 passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    console.log("JWT payload received:", jwt_payload);
     var user = db.find(jwt_payload.id);
-
     if (user) {
-        done(null, user);
+        return done(null, user);
     } else {
-        done(null, false);
+        console.log("User not found for ID:", jwt_payload.id);
+        return done(null, false);
     }
 }));
 
-exports.isAuthenticated = passport.authenticate('jwt', { session : false });
-exports.secret = opts.secretOrKey ;
+exports.isAuthenticated = passport.authenticate('jwt', { session: false });
+exports.secret = opts.secretOrKey;
